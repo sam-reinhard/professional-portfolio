d770ec6a3620bda00f8620d9e3093c3b0c167532
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-05-21 19:17:03 -0500

    fixed routes so that all work and all links work, fixed typo in portfolio.html file

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Serve static content
+app.use(express.static("Assets"));
 
 // Routes
 // =============================================================
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./assets/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
   
 app.get("/portfolio", function(req, res) {
-    res.sendFile(path.join(__dirname, "Assets/porfolio.html"));
+    res.sendFile(path.join(__dirname, "./assets/portfolio.html"));
 });
 
 app.get("/contact", function(req, res) {
-    res.sendFile(path.join(__dirname, "Assets/contact.html"));
+    res.sendFile(path.join(__dirname, "./assets/contact.html"));
 });
 
 // Starts the server

3fb4ef83446f6af37d57e64aa904ea8cda728012
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-05-22 18:15:29 -0500

    fixed a bug that prevented the navbar links from being clicked once a modal had been opened and closed

--- Assets/script/logic.js
+++ Assets/script/logic.js
@@ -1,21 +1,21 @@
-$(".project").on("click", function(e){
-    e.preventDefault();
-    var id = $(this).attr("data-id");
-    $("#" + id).show();
-
-    // Closing the modal options
-
-    // $(window).on("click", function(e){
-    //     e.preventDefault();
-    //     console.log("clicked on the window");
-
-    //     if (e.target == id) {
-    //         $("#" + id).hide();
-    //     }    
-    // });
-
-    $(".close").on("click", function(e){
+$(document).ready(function(){
+    $(".project").on("click", function(e){
         e.preventDefault();
-        $("#" + id).hide();
+        var id = $(this).attr("data-id");
+        $("#" + id).css("z-index", 1).show();
+
+        // Closing the modal options
+
+        // $(window).on("click", function(e){
+        //     e.preventDefault();
+        //     console.log("clicked on the window");
+        //     console.log(e.target);
+    
+        // });
+
+        $(".close").on("click", function(e){
+            e.preventDefault();
+            $("#" + id).css("z-index", -1).hide();
+        });
     });
 });

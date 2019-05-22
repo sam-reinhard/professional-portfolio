5c5b77f838fb7e43690fbcf855547df8498f8231
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-05-22 15:15:29 -0500

    added on click that closes the modals

--- Assets/script/logic.js
+++ Assets/script/logic.js
@@ -2,4 +2,20 @@ $(".project").on("click", function(e){
     e.preventDefault();
     var id = $(this).attr("data-id");
     $("#" + id).show();
+
+    // Closing the modal options
+
+    // $(window).on("click", function(e){
+    //     e.preventDefault();
+    //     console.log("clicked on the window");
+
+    //     if (e.target == id) {
+    //         $("#" + id).hide();
+    //     }    
+    // });
+
+    $(".close").on("click", function(e){
+        e.preventDefault();
+        $("#" + id).hide();
+    });
 });

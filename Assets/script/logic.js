aae10b1f1620c4b60c82e56bc98d4dea63507b05
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-05-30 18:35:03 -0500

    fixed modal scroll issues so that the page in the background doesn't scroll and the modal doesn't shift up and down when the user scrolls past the top/bottom, merged close button on-click with close span on-click

--- Assets/script/logic.js
+++ Assets/script/logic.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
+
+    // Project Box On-Click
     $(".project").on("click", function(e){
         e.preventDefault();
+
+        // Show the Modal
         var id = $(this).attr("data-id");
         $("#" + id).css("z-index", 1).show();
+        $("body").css("overflow", "hidden");
 
-        // Closing the modal options
-
-        $(".close").on("click", function(e){
+        
+        // Close the Modal
+        $(".close, .close-btn").on("click", function(e){
             e.preventDefault();
             $("#" + id).css("z-index", -1).hide();
+            $("body").css("overflow", "scroll");
         });
-    });
-
-    $(".close-btn").on("click", function(e){
-        e.preventDefault();
-        $(".modal").css("z-index", -1).hide();
-    })
+    });    
 });
